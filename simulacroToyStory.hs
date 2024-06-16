b06010f289d8ae7e9d6b40603c3eedf71bddce8b
Valentina <70960916+ValentinaArb@users.noreply.github.com>
2024-06-16 19:15:16 -0300

    simulacroToyStory terminado

--- simulacroToyStory.hs
+++ simulacroToyStory.hs
@@ -16,12 +16,16 @@ data Accesorio = UnAccesorio{
 
 type Efecto = Float -> Juguete -> Juguete
 
+cantidadCaracteresNombre :: Juguete -> Float
 cantidadCaracteresNombre= fromIntegral . length . nombre
+cantidadAccesorios :: Juguete -> Float
 cantidadAccesorios= fromIntegral . length . accesorios
 {- aplicacion parcial con "juguete" -}
 
 --1a
+modificarFacha :: Float -> Juguete -> Juguete
 modificarFacha cantidad juguete = juguete {nivelFacha = nivelFacha juguete + cantidad}
+modificarNombre :: String -> Juguete -> Juguete
 modificarNombre nuevoNombre juguete = juguete{nombre = nuevoNombre}
 
 lucirAmenazante :: Efecto
@@ -58,6 +62,9 @@ escopeta = UnAccesorio masSteel 20
 lanzaLlamas :: Accesorio
 lanzaLlamas = UnAccesorio (quemadura 3) 8.5
 
+pasaMontanias :: Accesorio
+pasaMontanias = UnAccesorio lucirAmenazante 7
+
 --3 a
 woody :: Juguete
 woody = UnJuguete "woody" "andy" 100 [serpienteEnBota,revolver,pasaMontanias] True
@@ -68,13 +75,13 @@ soldado = UnJuguete "soldado" "andy" 5 [lanzaLlamas,radio,pasaMontanias] True
 barbie :: Juguete
 barbie = UnJuguete "barbie" "danay" 95.5 [lanzaLlamas,escopeta,revolver,pasaMontanias] False
 
-eficaciaDeSusAccesorios juguete = map eficacia (accesorios juguete) 
-
 esImpactante :: Juguete -> Bool
 esImpactante juguete = any (>10) (eficaciaDeSusAccesorios juguete)
+    where eficaciaDeSusAccesorios juguete = map eficacia (accesorios juguete) 
 
 esDislexico :: Juguete -> Bool
 esDislexico juguete = sort (duenio juguete) /= sort "andy"
+cantidades :: (Juguete -> Bool) -> [Juguete] -> Int
 cantidades funcion = length . filter funcion
 {- aplicacion parcial con "juguetes" -}
 
@@ -84,8 +91,7 @@ cantidadImpactantes = cantidades esImpactante
 
 letrasNombreMayorA6 :: Juguete -> Bool
 letrasNombreMayorA6 = (>6) . cantidadCaracteresNombre
-{- aplicacion parcial con "juguetes" -}
-{- composición con point free -}
+{- aplicacion parcial con "juguetes" y composición con point free -}
 
 cantidadLetrasNombreMayorA6 :: [Juguete] -> Int
 cantidadLetrasNombreMayorA6 = cantidades letrasNombreMayorA6 
@@ -93,22 +99,19 @@ cantidadLetrasNombreMayorA6 = cantidades letrasNombreMayorA6
 
 cantidadDislexicosYNoVivos :: [Juguete] -> Int
 cantidadDislexicosYNoVivos = cantidades (not . estaVivo) . filter esDislexico
-{- aplicacion parcial con "juguetes" -}
 
-{- aplicarEfectoAccesorios :: Juguete -> Juguete -}
-aplicarEfectoAccesorios juguete accesorio = foldl (flip($)) (\funcion -> funcion (eficacia accesorio) juguete) (map efecto (accesorios juguete))
+aplicarAccesorios :: Juguete -> Juguete
+aplicarAccesorios juguete = foldl aplicarAccesorio juguete (accesorios juguete)
+    where aplicarAccesorio juguete accesorio = efecto accesorio (eficacia accesorio) juguete
 
 formulaAmorPorJuguete :: Juguete -> Float
 formulaAmorPorJuguete juguete = nivelFacha juguete + cantidadCaracteresNombre juguete * 5 - cantidadAccesorios juguete * 7
     
 amorAndyPorJuguete :: Juguete -> Float
 amorAndyPorJuguete juguete 
-    |estaVivo juguete = (2*) . formulaAmorPorJuguete . aplicarEfectoAccesorios $ juguete
-    |otherwise = formulaAmorPorJuguete . aplicarEfectoAccesorios $ juguete
-
-pasaMontanias :: Accesorio
-pasaMontanias = UnAccesorio lucirAmenazante 7
+    |estaVivo juguete = (2*) . formulaAmorPorJuguete . aplicarAccesorios $ juguete
+    |otherwise = formulaAmorPorJuguete . aplicarAccesorios $ juguete
 
 amorCajon :: [Juguete] -> Float
 amorCajon = sum . map amorAndyPorJuguete
-{- aplicacion parcial con "juguetes" -}
+{- aplicacion parcial con "juguetes" y orden superior en el map -}
