4fdc21b75084842e8f83b98717cfc6e376dac4bc
Valentina <70960916+ValentinaArb@users.noreply.github.com>
2024-08-24 00:08:22 -0300

    ui

--- Logico/delivery.pl
+++ Logico/delivery.pl
@@ -17,16 +17,36 @@ proveedor(sanIgnacio, [zanahoria, lechuga, miel, huevo]).
 /* 1) caloriasTotal/2, que relaciona un plato con su cantidad total de calorías por porción. 
 Las calorías de un plato se calculan a partir de sus ingredientes.
 P.ej. la milanesa tiene 206 calorías (30 * 3 + 18 * 2 + 40 * 2) por porción. */
-
-my_sum_elements([], 0).
-
-my_sum_elements([X|Xs], S):-
-          my_sum_elements(Xs, S2),
-          S is S2 + X.
-
-caloriasTotal(composicion(_,Lista),CantidadCalorias):- 
-    findall(member(Ingrediente,Lista),caloriasIngrediente(Ingrediente,_),ListaCalorias),    
-    my_sum_elements(ListaCalorias, CantidadCalorias).
-
+caloriasTotales(Plato, CaloriasTotales) :-
+    composicion(Plato, Ingredientes),
+    findall(Calorias, 
+        (member(Ingrediente, Ingredientes), 
+            caloriasIngrediente(Ingrediente, Calorias)), 
+        ListaCalorias),
+        sum_list(ListaCalorias, CaloriasTotales).
 
 caloriasIngrediente(ingrediente(Comida,Cantidad),CaloriasIngrediente):- calorias(Comida,Caloria), CaloriasIngrediente is Caloria * Cantidad.
+
+/* 3) platoSimpatico/1
+Se dice que un plato es simpático si ocurre alguna de estas condiciones:
+· incluye entre sus ingredientes al pan y al huevo.
+· tiene menos de 200 calorías por porción.
+En el ejemplo, la milanesa es simpática, mientras que el budín de pan no (tiene pan pero no
+huevo, y tiene 230 calorías por porción).
+Asegurar que el predicado sea inversible. */
+/* platoSimpatico(Plato):-  */
+
+incluyeIngrediente(Plato,Ingrediente):- 
+    composicion(Plato,ListaIngredientes),
+    member(ingrediente(Ingrediente,_),ListaIngredientes).
+
+tieneMenosDeXCalorias(X,Plato):-
+    caloriasTotales(Plato, CaloriasTotales),
+    CaloriasTotales<X.
+
+platoSimpatico(Plato):-
+    incluyeIngrediente(Plato,pan),
+    incluyeIngrediente(Plato,huevo).
+
+platoSimpatico(Plato):- tieneMenosDeXCalorias(200,Plato).
+    
